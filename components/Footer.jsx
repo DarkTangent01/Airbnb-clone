6739c7bd3bbd56d23df782f24fd13d2e553bc28f
DarkTangent01 <mayankksrivastavaa@gmail.com>
2021-10-03 14:14:05 +0530

    airbnb clone

---
+++ components/Footer.jsx
@@ -0,0 +1,45 @@
+const Footer = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600 ">
+
+        
+      <div className="space-y-4 text-gray-800 text-xs">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb Works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+
+      <div className="space-y-4 text-gray-800 text-xs">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>This is not a real site</p>
+        <p>Its a pretty awesome clone</p>
+        <p>Referrals accepted</p>
+        <p>Airbnb Clone</p>
+      </div>
+
+      <div className="space-y-4 text-gray-800 text-xs">
+        <h5 className="font-bold">HOST</h5>
+        <p>Reactjs Nextjs</p>
+        <p>Presents</p>
+        <p>Zero to Hero</p>
+        <p>Become a Host</p>
+        <p>Find Rooms</p>
+      </div>
+
+      <div className="space-y-4 text-gray-800 text-xs">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Help Center</p>
+        <p>Trust & Safety</p>
+        <p>Say Hi to Airbnb Clone</p>
+        <p>Easter Eggs</p>
+        <p>For the Win</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
